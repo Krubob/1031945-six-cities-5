72529943ebb83fddc9b4431ef86a3ecd0d605568
Vladislav <res.19@yandex.ru>
2020-10-08 18:14:26 +0300

    Добавляет новые компоненты, добавляет маршрутизацию

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,32 @@
 import React from "react";
-import Main from "../main/main";
 import PropTypes from 'prop-types';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Main from "../main/main";
+import Favorites from "../favorites/favorites";
+import Offer from "../offer/offer";
+import Login from "../login/login";
 
 const App = (props) => {
 
   const {offersNumber} = props;
+
   return (
-    <Main offersNumber={offersNumber} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main offersNumber={offersNumber} />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/offer/:id">
+          <Offer />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 

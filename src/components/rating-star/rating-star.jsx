5387b65a4ebb50626b16f09f410ab8810b812bb5
Vladislav <res.19@yandex.ru>
2020-10-19 19:16:30 +0300

    Добавляет правки в компонент RatingStar

--- src/components/rating-star/rating-star.jsx
+++ src/components/rating-star/rating-star.jsx
@@ -1,48 +1,34 @@
-import React, {PureComponent, Fragment} from "react";
+import React, {Fragment} from "react";
 import PropTypes from "prop-types";
 import {StarPropTypes} from "../../propTypes";
 
-class RatingStar extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
+const RatingStar = (props) => {
+  const {star, onInputStarClick, checked} = props;
 
-  handleInputStarChange() {
-    const radio = this.input.current;
-    this.setState(() => {
-      return {checked: this.input.current.checked};
-    });
-    radio.setAttribute(`checked`, !this.state.checked);
-  }
-
-  render() {
-    const {star, onInputStarClick, checked} = this.props;
-
-    return (
-      <Fragment>
-        <input
-          className="form__rating-input visually-hidden"
-          name="rating"
-          value={star.value}
-          id={`${star.value}-stars`}
-          type="radio"
-          onChange={onInputStarClick}
-          checked = {checked}
-        />
-        <label htmlFor={`${star.value}-stars`} className="reviews__rating-label form__rating-label" title={star.title}>
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-      </Fragment>
-    );
-  }
-}
+  return (
+    <Fragment>
+      <input
+        className="form__rating-input visually-hidden"
+        name="rating"
+        value={star.value}
+        id={`${star.value}-stars`}
+        type="radio"
+        onChange={onInputStarClick}
+        checked = {checked}
+      />
+      <label htmlFor={`${star.value}-stars`} className="reviews__rating-label form__rating-label" title={star.title}>
+        <svg className="form__star-image" width="37" height="33">
+          <use xlinkHref="#icon-star"></use>
+        </svg>
+      </label>
+    </Fragment>
+  );
+};
 
 RatingStar.propTypes = {
   onInputStarClick: PropTypes.func.isRequired,
   star: StarPropTypes.isRequired,
-  checked: PropTypes.bool,
+  checked: PropTypes.bool.isRequired,
 };
 
 export default RatingStar;

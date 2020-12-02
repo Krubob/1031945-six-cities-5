a5a58e89c198bf5bb096db98a0e7cb2696603bef
Vladislav <res.19@yandex.ru>
2020-12-02 15:04:42 +0300

    добавляет правки в e2e тест для SortingItem

--- src/components/sorting-item/sorting-item.e2e.test.js
+++ src/components/sorting-item/sorting-item.e2e.test.js
@@ -6,16 +6,19 @@ import {SortingType} from "../../const";
 
 configure({adapter: new Adapter()});
 
-test(`click on SortingItem correctly`, () => {
-  const onSortingClick = jest.fn();
-  const wrapper = shallow(
-      <SortingItem
-        type={SortingType.POPULAR}
-        activeSorting={SortingType.TOP_RATED}
-        onSortingClick={onSortingClick}
-      />
-  );
+describe(`e2e test: SortingType component`, () => {
+  it(`should call onSortingClick with type`, () => {
+    const onSortingClick = jest.fn();
+    const wrapper = shallow(
+        <SortingItem
+          type={SortingType.TOP_RATED}
+          activeSorting={SortingType.TOP_RATED}
+          onSortingClick={onSortingClick}
+        />
+    );
 
-  wrapper.find(`li.places__option`).simulate(`click`);
-  expect(onSortingClick).toHaveBeenCalledTimes(1);
+    wrapper.find(`li.places__option`).simulate(`click`);
+    expect(onSortingClick).toHaveBeenCalledTimes(1);
+    expect(onSortingClick.mock.calls[0][0]).toBe(SortingType.TOP_RATED);
+  });
 });
